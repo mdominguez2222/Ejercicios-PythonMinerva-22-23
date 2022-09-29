78e0426a9d049f1aa84dee20df3ceda5b007ba28
Minerva <al_mpacod@iessierradelasvillas.org>
2022-09-29 10:06:49 +0200

    Comentarios, entrada

---
+++ hola_mundo.py
@@ -0,0 +1,21 @@
+#Tipos de datos y variables
+
+#Numéricos
+#enteros -- int
+#real -- float
+#Cadenas de caracteres -- str
+#Lógico -- bool
+
+nombre="Minerva"
+edad = 16
+mayorOmenorEdad=False
+
+#Entrada y salida de datos
+#Salida con print
+
+
+#Entrada de datos -- input()
+nombre = input("Dime tu nombre:\n") #\n Permite hacer un salto de línea
+edad = input("¿Cuál es tu edad?: ")
+
+print("Buenos días",nombre, "tu edad es",edad)
